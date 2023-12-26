360c0ab870b79255a2e640d01435386b4e50e88f
Ben Hmida Aymen <118215572+AymenBH52@users.noreply.github.com>
2023-12-26 17:12:45 +0100

    1)L'ajouts des photos utilisables sous "drawable" 2)L'ajout des couleurs utilisables dans "colors.xml sous dossier "values" 3)Créaton des interfaces {activity_login, activity_register, multi_lines} 4)Développemnt des layouts {LabTestDetailsActivity, LabTestActivity, CartLabActivity, BookAppointmentActivity}

---
+++ app/src/main/java/com/tekup/cabinetmedicale/CartLabActivity.java
@@ -0,0 +1,159 @@
+package com.tekup.cabinetmedicale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class CartLabActivity extends AppCompatActivity {
+
+    HashMap<String, String> item;
+    ArrayList<HashMap<String, String>> list; // Correction du type de données
+    SimpleAdapter sa;
+    TextView tvTotal;
+    ListView lst;
+    private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
+    private Button dateButton, timeButton, btnCheckout, btnBack;
+    private String[][] packages = {};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_lab);
+
+        dateButton = findViewById(R.id.buttonCartDate);
+        timeButton = findViewById(R.id.buttonCartTime);
+        btnCheckout = findViewById(R.id.buttonCartCheckout);
+        btnBack = findViewById(R.id.buttonCartBack);
+        tvTotal = findViewById(R.id.textViewCartTotalCost);
+        lst = findViewById(R.id.listViewCart);
+
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+/*
+        Database db = new Database(getApplicationContext(), "healthcare", null, 1);
+
+        float totalAmount = 0;
+        ArrayList<HashMap<String, String>> dbData = db.getCartData(username, "lab"); // Correction du type de données
+        Toast.makeText(getApplicationContext(), "" + dbData, Toast.LENGTH_LONG).show();
+
+        packages = new String[dbData.size()][];
+        for(int i=0; i<packages.length; i++){
+            packages[i] = new String[5];
+        }
+
+        for(int i=0; i<dbData.size();i++) {
+            String arrData = dbData.get(i).toString();
+            String[] strData = arrData.split((java.util.regex.Pattern.quote("$"));
+            packages[i][0] = strData[0];
+            packages[i][4] = "Cost : "+strData[1]+"/-";
+            totalAmount = totalAmount + Float.parseFloat(strData[1]);
+        }
+        tvTotal.setText("Total Cost : "+totalAmount); */
+
+        list = new ArrayList<>();
+        for(int i=0; i<packages.length ; i++){
+            item = new HashMap<String, String>();
+            item.put("line1", packages[i][0]);
+            item.put("line2", packages[i][1]);
+            item.put("line3", packages[i][2]);
+            item.put("line4", packages[i][3]);
+            item.put("line5", "Total Cost:" +packages[i][4]+"/-");
+            list.add(item);
+        }
+
+        sa = new SimpleAdapter(this, list,
+                R.layout.multi_lines,
+                new String[] {"line1", "line2", "line3", "line4", "line5" },
+                new int[] {R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e});
+        lst.setAdapter(sa);
+
+
+        // Événements des boutons :
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CartLabActivity.this, LabTestActivity.class));
+            }
+        });
+
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CartLabActivity.this, LabTestBookActivity.class));
+            }
+        });
+
+        // Appel des fonctions "initDatePicker()" et "initTimePicker()" :
+        initDatePicker();
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+        initTimePicker();
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog.show();
+            }
+        });
+    }
+
+    // Initialisation de date :
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                i1 = i1 + 1;
+                dateButton.setText(i2 + "/" + i1 + "/" + i);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMinDate(cal.getTimeInMillis() + 86400000);
+    }
+
+    // Initialisation de l'horloge :
+    private void initTimePicker() {
+        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                timeButton.setText(i + ":" + i1);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+
+        timePickerDialog = new TimePickerDialog(this, timeSetListener, hour, minute, true);
+    }
+}
